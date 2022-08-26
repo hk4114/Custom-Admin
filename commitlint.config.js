1dc81f8a4fa62c164668d88338610eb8fba2c15a
huakang <741180788@qq.com>
2022-08-26 16:59:54 +0800

    fix: commitlint desc

--- commitlint.config.js
+++ commitlint.config.js
@@ -46,17 +46,6 @@ module.exports = {
 			customFooterPrefixs: "Input ISSUES prefix:",
 			footer: "List any ISSUES by this change. E.g.: #31, #34:\n",
 			confirmCommit: "Are you sure you want to proceed with the commit above?"
-			// 中文版
-			// type: "选择你要提交的类型 :",
-			// scope: "选择一个提交范围（可选）:",
-			// customScope: "请输入自定义的提交范围 :",
-			// subject: "填写简短精炼的变更描述 :\n",
-			// body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
-			// breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-			// footerPrefixsSelect: "选择关联issue前缀（可选）:",
-			// customFooterPrefixs: "输入自定义issue前缀 :",
-			// footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
-			// confirmCommit: "是否提交或修改commit ?"
 		},
 		types: [
 			{
@@ -114,18 +103,6 @@ module.exports = {
 				name: "revert:   ⏪️  Reverts a previous commit",
 				emoji: "⏪️"
 			}
-			// 中文版
-			// { value: "特性", name: "特性:   🚀  新增功能", emoji: "🚀" },
-			// { value: "修复", name: "修复:   🧩  修复缺陷", emoji: "🧩" },
-			// { value: "文档", name: "文档:   📚  文档变更", emoji: "📚" },
-			// { value: "格式", name: "格式:   🎨  代码格式（不影响功能，例如空格、分号等格式修正）", emoji: "🎨" },
-			// { value: "重构", name: "重构:   ♻️  代码重构（不包括 bug 修复、功能新增）", emoji: "♻️" },
-			// { value: "性能", name: "性能:   ⚡️  性能优化", emoji: "⚡️" },
-			// { value: "测试", name: "测试:   ✅  添加疏漏测试或已有测试改动", emoji: "✅" },
-			// { value: "构建", name: "构建:   📦️  构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）", emoji: "📦️" },
-			// { value: "集成", name: "集成:   🎡  修改 CI 配置、脚本", emoji: "🎡" },
-			// { value: "回退", name: "回退:   ⏪️  回滚 commit", emoji: "⏪️" },
-			// { value: "其他", name: "其他:   🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）", emoji: "🔨" }
 		],
 		useEmoji: true,
 		themeColorCode: "",
